700950dd5f4eac44a872da8eca7ef90c95a62c28
angel.chiveg <angel.chiveg@educa.jcyl.es>
2025-01-29 14:19:09 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:repaso_flutter/presentation/views/home.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      home: const Home(),
+    );
+  }
+}
